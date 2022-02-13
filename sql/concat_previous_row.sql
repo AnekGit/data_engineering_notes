f55975f88959e26e9a8d867800c6a74693bc5655
AnekGit <anekkumarsingh01@outlook.com>
2022-02-13 21:05:26 +0530

    concat previous row using list_agg and instr function

---
+++ sql/concat_previous_row.sql
@@ -0,0 +1,33 @@
+--concat_previous_row
+-- youtube : Siva Academy => https://www.youtube.com/watch?v=bDoRdi7T3u8&t=24s
+
+create table concat(col1 varchar2(10),col2 varchar2(10));
+insert into concat values ('A',1);
+insert into concat values ('A',2);
+insert into concat values ('A',3);
+insert into concat values ('B',100);
+insert into concat values ('B',200);
+insert into concat values ('B',300);
+insert into concat values ('B',400);
+
+select * from concat;
+
+with ds as (
+            select 
+            a.col1,
+            a.col2,
+            listagg(a.col2, ',') within group (order by a.col2) over (partition by a.col1) as agg,
+            row_number() over (partition by a.col1 order by a.col2) as occ 
+            from concat a
+)
+select col1 ,col2 , agg, occ ,
+instr(agg||',' , ',' ,1,occ) comma_occ,
+-- append comma so that in the comma_occ there should not be any zero and our results are streamlined 
+-- debug prev step then revise 
+-- 1st row => first occ of comma  2nd row => 2nd occ of comma
+substr(agg,1,instr(agg||',' , ',' ,1,occ)-1)
+from ds ; 
+
+
+
+
